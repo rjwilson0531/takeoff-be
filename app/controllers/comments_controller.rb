98931013ab612d069cebee9c9763cd5c370a11ed
rjwilson0531 <rjwilson0531@gmail.com>
2020-04-30 16:41:56 -0400

    comments done on be

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,10 @@
 class CommentsController < ApplicationController
 
+  def index
+    comments = Comment.where(:post_id => params["post_id"])
+    render json: comments.as_json()
+  end
+  
   def create
     comment = Comment.create(content: params["content"], user_id: params["user_id"], post_id: params["post_id"])
     render json: comment.as_json()
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :create, :destroy] do
-    resources :comments, only: [:create, :destroy]
+    resources :comments, only: [:index, :create, :destroy]
   end
   resources :likes, only: [:create, :destroy]
 end
